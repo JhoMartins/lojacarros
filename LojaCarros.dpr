8670487e55cbd38a9b13a2b4fa60326d6590ac07
Jhonatan Martins <jhonatanbmartins@gmail.com>
2017-11-15 16:50:11 -0200

    Remove forms de cliente que nao gherdam do framework

--- LojaCarros.dpr
+++ LojaCarros.dpr
@@ -3,9 +3,7 @@ program LojaCarros;
 uses
   Vcl.Forms,
   UntMenuPrincipal in 'UntMenuPrincipal.pas' {Form1},
-  UntManCliente in 'UntMan\UntManCliente.pas' {FrmManCliente},
   UntDM in 'bd\UntDM.pas' {DM: TDataModule},
-  UntCadCliente in 'UntCad\UntCadCliente.pas' {FrmCadCliente},
   UntManBase in 'UntMan\UntManBase.pas' {FrmManBase},
   UntManEmpresa in 'UntMan\UntManEmpresa.pas' {FrmManEmpresa},
   UntCadBase in 'UntCad\UntCadBase.pas' {FrmCadBase},
@@ -19,9 +17,7 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TFrmManCliente, FrmManCliente);
   Application.CreateForm(TDM, DM);
-  Application.CreateForm(TFrmCadCliente, FrmCadCliente);
   Application.CreateForm(TFrmManBase, FrmManBase);
   Application.CreateForm(TFrmManEmpresa, FrmManEmpresa);
   Application.CreateForm(TFrmCadBase, FrmCadBase);
