0ab800d8fb2077d32754dd6017699dc1bceaa596
dualshockmaster <xdeividsabinox@live.com>
2017-11-15 15:38:27 -0200

    Criação do menu principal

--- UntMenuPrincipal.pas
+++ UntMenuPrincipal.pas
@@ -4,12 +4,31 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, UntManCliente;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, UntManCliente,
+  Vcl.ActnCtrls, Vcl.ToolWin, Vcl.ActnMan, Vcl.ActnMenus, System.Actions,
+  Vcl.ActnList, Vcl.PlatformDefaultStyleActnCtrls, System.ImageList, Vcl.ImgList,
+  Vcl.ComCtrls, Vcl.ExtCtrls;
 
 type
-  TForm1 = class(TForm)
+  TFrmMenuPrincipal = class(TForm)
     Button1: TButton;
+    ImageList1: TImageList;
+    cad_cliente: TAction;
+    cad_carro: TAction;
+    cad_empresa: TAction;
+    ActionMainMenuBar1: TActionMainMenuBar;
+
+    sair: TAction;
+    ActionToolBar1: TActionToolBar;
+    ActionManager1: TActionManager;
+    ActionList1: TActionList;
+    StatusBar1: TStatusBar;
+    Timer1: TTimer;
     procedure Button1Click(Sender: TObject);
+    procedure sairExecute(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+    procedure Timer1Timer(Sender: TObject);
   private
     { Private declarations }
   public
@@ -17,15 +36,48 @@ type
   end;
 
 var
-  Form1: TForm1;
+  FrmMenuPrincipal: TFrmMenuPrincipal;
 
 implementation
 
 {$R *.dfm}
 
-procedure TForm1.Button1Click(Sender: TObject);
+uses UntCadCliente, UntDM;
+
+procedure TFrmMenuPrincipal.Button1Click(Sender: TObject);
 begin
   FrmManCliente.ShowModal;
+end;
+
+procedure TFrmMenuPrincipal.FormActivate(Sender: TObject);
+begin
+windowstate :=  wsMaximized;
+
+statusbar1.panels[1].text:= formatdatetime(' dddd ", " dd " de " mmmm " de " yyyy', now);
+end;
+
+procedure TFrmMenuPrincipal.FormCloseQuery(Sender: TObject;
+  var CanClose: Boolean);
+  var confSaida: integer;
+begin
+   confSaida:= application.MessageBox('Tem certeza que deseja sair do sistema?',
+   'Atenção', MB_YESNO+MB_DEFBUTTON2+MB_ICONQUESTION);
+
+   if confSaida = IDYES then
+    application.Terminate
+   else
+    canclose:=false;
+
+end;
+
+procedure TFrmMenuPrincipal.sairExecute(Sender: TObject);
+begin
+close;
+end;
+
+procedure TFrmMenuPrincipal.Timer1Timer(Sender: TObject);
+begin
+statusbar1.panels[0].Text:= timetostr(time);
 end;
 
 end.
