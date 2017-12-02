33ea494390292cc7988ab5dcf3ce9881d476f211
Jhonatan Martins <jhonatanbmartins@gmail.com>
2017-12-02 14:08:45 -0200

    melhoria no layout de funcionario e remoção de campos desnecessarios

--- UntCad/UntCadFuncionario.pas
+++ UntCad/UntCadFuncionario.pas
@@ -19,9 +19,6 @@ type
     Label8: TLabel;
     Label9: TLabel;
     Label10: TLabel;
-    Label11: TLabel;
-    Label12: TLabel;
-    Label13: TLabel;
     Label14: TLabel;
     Label15: TLabel;
     DBEdit1: TDBEdit;
@@ -33,9 +30,6 @@ type
     DBComboBox1: TDBComboBox;
     DBEdit7: TDBEdit;
     DBEdit8: TDBEdit;
-    DBEdit9: TDBEdit;
-    DBComboBox2: TDBComboBox;
-    DBEdit10: TDBEdit;
     DBEdit11: TDBEdit;
     DBEdit12: TDBEdit;
   private
